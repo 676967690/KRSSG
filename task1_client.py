5fa1691a3a6fc6ea4348b34a5802bcd1585b8933
Sumandas500 <107878213+Sumandas500@users.noreply.github.com>
2022-06-27 21:23:51 +0530

    Update task1_client.py

--- task1_client.py
+++ task1_client.py
@@ -1,6 +1,7 @@
 import socket               # Import socket module
 import random,os,time
-for i in range(4000):
+i=0
+while i == 0:
     s = socket.socket()         # Create a socket object
     host = socket.gethostname() # Get local machine name
     port = 1245 
@@ -8,5 +9,7 @@ for i in range(4000):
     msg = (str)(random.randint(1,6))
 
     s.send(msg.encode("utf-8"))
+    i = s.recv(1024).decode('utf-8')
+    i = int(i)
     time.sleep(0.001)
     s.close()
